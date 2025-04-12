765af4a6e14b1fc1c0d4c0bbd5fe7397dbc068a1
Ehsan Gudarzi <ehsan.gdrzi@gmail.com>
2025-04-12 10:58:28 +0330

    Fix some remaining merge issues

--- SaveHere/SaveHere/Migrations/20250412071925_InitialCreate.Designer.cs
+++ SaveHere/SaveHere/Migrations/20250412071925_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SaveHere.Models.db;
 
@@ -10,9 +11,11 @@ using SaveHere.Models.db;
 namespace SaveHere.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250412071925_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.14");
